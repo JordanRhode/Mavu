2d5efaf2baaa334b2c974e1a76b4e68b99d60d3d
Jordan Rhode <rhode.jordan@gmail.com>
2012-12-11 15:12:18 -0600

    final PHP file

--- server_files/user_actions.php
+++ server_files/user_actions.php
@@ -4,19 +4,18 @@
 
 	if(isset($_REQUEST["action"]))
 	{
-                switch ($_REQUEST["action"]) {
+        switch ($_REQUEST["action"]) {
 			case "Login":
 				if(isset($_REQUEST["email"])
 					and isset($_REQUEST["password"]))
 				{
 					$password = sha1($_REQUEST["password"] . $salt);
 
-					$sql = "SELECT account_id, name " .
+					$sql = "SELECT account_id, first_name, last_name, email, dob, likes, dislikes " .
 							"FROM mavu_account " .
 							"WHERE email='" . $_REQUEST["email"] . "' " .
 							"AND password='" . $password . "'";
-					$result = mysql_query($sql, $conn)
-						or die("Couldn't look up user information: " . mysql_error());
+					$result = mysql_query($sql, $conn);
 					if($row = mysql_fetch_array($result))
 					{
 						$output[] = $row;
@@ -72,7 +71,6 @@
 			    	and isset($_REQUEST["account_id"]))
 			    {
 			        $password = sha1($_REQUEST["password"] . $salt);
-
 			     	$sql = "UPDATE mavu_account " .
 			     			"SET first_name='" . $_REQUEST["fname"] .
 			     			"', last_name='" . $_REQUEST["lname"] .
@@ -91,20 +89,21 @@
                                     );
 						}
 						$output = array();
-			    	array_push($output, $bus);
-			    	print(json_encode($output));
+				    	array_push($output, $bus);
+				    	print(json_encode($output));
 			    }
 				break;
 			case "Get Account":
 				if(isset($_REQUEST["account_id"])){
-					$sql = "SELECT first_name, last_name, email, dob, likes, dislikes " .
+					$sql = "SELECT account_id, first_name, last_name, email, dob, likes, dislikes " .
 							"FROM mavu_account " .
 							"WHERE account_id='" . $_REQUEST["account_id"] . "'";
-					$result = mysql_query($sql, $conn) or die(mysql_error());
+					$result = mysql_query($sql, $conn);
 					$output = array();
 					while($row = mysql_fetch_array($result))
 					{
 						$bus = array(
+									'accountID' => $row['account_id'],
                                     'first_name' => $row['first_name'],
                                     'last_name' => $row['last_name'],
                                     'email' => $row['email'],
@@ -124,63 +123,79 @@
 						and isset($_REQUEST["city"])
 						and isset($_REQUEST["time"])
 						and isset($_REQUEST["date"])
-						and isset($_REQUEST["address"])
-						and isset($_REQUEST["zipcode"]))
+						and isset($_REQUEST["address"]))
 					{
 						$sql = "INSERT INTO mavu_post(account_id, title, description, category, city, time, date, address, zipcode) " .
-								"VALUES('" . $_SESSION["account_id"] . "', '" . $_REQUEST["title"] . "', '" . $_REQUEST["description"] . 
-									"', '" . $_REQUEST["category"] . "', '" . strtoupper(($_REQUEST["city"])) . "', '" . $_REQUEST["time"] .
-									"', '" . $_REQUEST["date"] . "', '" . $_REQUEST["address"] . "', '" . $_REQUEST["zipcode"] . "')";
-						mysql_query($sql, $conn)
-								or die("Couldn't create post: " + mysql_error());
+								"VALUES('" . $_REQUEST["account_id"] . "', '" . $_REQUEST["title"] . "', '" . $_REQUEST["description"] . 
+									"', '" . $_REQUEST["category"] . "', '" . ($_REQUEST["city"]) . "', '" . $_REQUEST["time"] .
+									"', '" . $_REQUEST["date"] . "', '" . $_REQUEST["address"] . "', '" . "12345" . "')";
+						echo $sql;
+						$result = mysql_query($sql, $conn);
+						if($result){
+							$bus = array(
+                                    'post' => 'created'
+                                    );
+						} else {
+							$bus = array(
+                                    'post' => 'failed'
+                                    );
+						}
+						$output = array();
+				    	array_push($output, $bus);
+				    	print(json_encode($output));
 					}
 					break;
 			case "Get Posts":
                         if(isset($_REQUEST["lowDate"])
 					and isset($_REQUEST["highDate"])
-					and isset($_REQUEST["city"])
 					and isset($_REQUEST["music"])
 					and isset($_REQUEST["business"])
 					and isset($_REQUEST["food"]))
 				{
                                      
-					$sql = "SELECT post_id, title, description, category, city, time, date, address " .
+					$sql = "SELECT post_id, title, description, category, city, time, date, address, account_id " .
 					"FROM mavu_post " .
-					"WHERE date BETWEEN '" . $_REQUEST["lowDate"] . "' and '" . $_REQUEST["highDate"] . "' " .
-					"AND city='" . strtoupper($_REQUEST["city"]) . "' ";
-                                        //echo $sql;
+					"WHERE date BETWEEN '" . $_REQUEST["lowDate"] . "' and '" . $_REQUEST["highDate"] . "' ";
+					if(isset($_REQUEST["city"]))
+					{
+						$sql = $sql . "AND UPPER(city)='" . strtoupper($_REQUEST["city"]) . "' ";
+					}
 					if($_REQUEST["music"] == "true" && $_REQUEST["business"] == "true" && $_REQUEST["food"] == "true")
 					{
-						$category = " AND category IN ('music', 'business', 'food') ";
+						$category = " AND category IN ('Music', 'Business', 'Food') ";
 					}
 					else if($_REQUEST["music"] == "true" && $_REQUEST["business"] == "false" && $_REQUEST["food"] == "true")
 					{
-						$category = " AND category IN ('music', 'food') ";
+						$category = " AND category IN ('Music', 'Food') ";
 					}
 					else if($_REQUEST["music"] == "true" && $_REQUEST["business"] == "false" && $_REQUEST["food"] == "false")
 					{
-						$category = " AND category IN ('music') ";
+						$category = " AND category IN ('Music') ";
 					}
 					else if($_REQUEST["music"] == "true" && $_REQUEST["business"] == "true" && $_REQUEST["food"] == "false")
 					{
-						$category = " AND category IN ('music', 'business') ";
+						$category = " AND category IN ('Music', 'Business') ";
 					}
 					else if($_REQUEST["music"] == "false" && $_REQUEST["business"] == "true" && $_REQUEST["food"] == "true")
 					{
-						$category = " AND category IN ('business', 'food') ";
+						$category = " AND category IN ('Business', 'Food') ";
 					}
 					else if($_REQUEST["music"] == "false" && $_REQUEST["business"] == "true" && $_REQUEST["food"] == "false")
 					{
-						$category = " AND category IN ('business') ";
+						$category = " AND category IN ('Business') ";
 					}
 					else if($_REQUEST["music"] == "false" && $_REQUEST["business"] == "false" && $_REQUEST["food"] == "true")
 					{
-						$category = " AND category IN ('food')";
+						$category = " AND category IN ('Food')";
+					}
+					else if($_REQUEST["music"] == "false" && $_REQUEST["business"] == "false" && $_REQUEST["food"] == "false")
+					{
+						$category = " AND category NOT IN ('Food', 'Business', 'Music') ";
 					}
 					$sql = $sql . $category;
                                         
 					if(isset($_REQUEST["title"])){
-						$sql = $sql . " AND title='" . $_REQUEST["title"] . "'";
+						$sql = $sql . " AND UPPER(title)='" . strtoupper($_REQUEST["title"]) . "'";
 					}
                                         
 					$result = mysql_query($sql, $conn) or die(mysql_error());
@@ -195,14 +210,13 @@
                                                         'city' => $row['city'],
                                                         'time' => $row['time'],
                                                         'date' => $row['date'],
-                                                        'address' => $row['address']
-                                                        ']
+                                                        'address' => $row['address'],
+                                                        'accountID' => $row['account_id']
                                                     );
                                                 array_push($output, $bus);
                                         }
                                         print(json_encode($output));
                                 }
-			mysql_close();
 			break;
 		}
 		mysql_close();
